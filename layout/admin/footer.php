a8cb9774a624786c9315e5dca4f5eaf453ae3b54
Erikson <perezerikson16gmail.com>
2024-06-12 16:35:20 -0400

    plazaParking

---
+++ layout/admin/footer.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2024-2030 <a href="">AdminLTE.io</a>.</strong> Erikson 2019-0291, JHONATAN HERNANDEZ 2022-0023 
+</footer>
